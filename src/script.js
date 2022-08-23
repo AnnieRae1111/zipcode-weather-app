8f7ec73f728e32f46c0d0a0742d7094823ceccb4
AnnieRae1111 <annierae.elizabeth@gmail.com>
2022-08-23 15:45:23 -0600

    zipcode validator function

--- src/script.js
+++ src/script.js
@@ -8,3 +8,21 @@ let zipcode;
 let regex = /^(\d{5})?$/;
 let error = document.querySelector('.error');
 let submitButton = document.querySelector('.submit-button');
+
+const getZipCode = () => {
+  let zipcode = document.querySelector('input').value;
+  console.log(zipcode, 'zipcode');
+  console.log(zipcode.length);
+  return zipcode;
+};
+
+const validateZip = () => {
+  let zip = document.getElementById('zipcode').value;
+  let alert = document.getElementById('error');
+  if (!regex.test(zip)) {
+    alert.style.display = 'block';
+    alert.innerHTML = 'Zip code must be 5 digits';
+  } else {
+    alert.style.display = 'none';
+  }
+};
